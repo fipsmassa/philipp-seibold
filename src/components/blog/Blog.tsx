68bd4355998835ea180cf6de5c2ed6616339f85b
Philipp <phil.seibold94@gmail.com>
2024-12-18 15:18:29 +0100

    added link to blog

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import EngineeringIcon from "@mui/icons-material/Engineering";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 import "./blog.css";
 
 export const Blog = () => {
   return (
     <section className="blog" id="blog">
       <div className="container">
-        <h2 className="container_heading">My Blog</h2>
+        <h2 className="container_heading">
+          My Blog (or Docs, whatever you want to call it)
+        </h2>
         <div className="blog_content">
-          <h3>Currently under construction</h3>
-          <EngineeringIcon className="blog_icon" />
+          <h3>Check it out!</h3>
+          <a href="https://blog.philipp-seibold.de">
+            <RocketLaunchIcon className="blog_icon" />
+          </a>
         </div>
       </div>
     </section>
